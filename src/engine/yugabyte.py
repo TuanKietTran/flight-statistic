66bb4225f4bc00ea1069af284ed2f5edec8dd00b
La Minh Duc <laminhduc200@gmail.com>
2024-11-11 01:57:17 +0700

    Create a method insert bulk data

--- src/engine/yugabyte.py
+++ src/engine/yugabyte.py
@@ -1,5 +1,6 @@
 # yugabyte.py - Define functions to interact with YugabyteDB
 import psycopg2
+from io import StringIO
 from .config import get_config
 
 config = get_config()
@@ -49,3 +50,27 @@ def insert_data(flight_number, year, month, day, dep_time, arr_time, origin, des
     cursor.close()
     conn.close()
     print(f"Data for flight '{flight_number}' inserted successfully.")
+    
+def insert_batch_data(file):
+    conn = get_warehouse_connection()
+    cursor = conn.cursor()
+    cursor.copy_expert("COPY flight_data FROM STDIN DELIMITER ',' CSV HEADER", file)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Table 'flight_data' created successfully.")
+    
+def df2filestream(df):
+    list_row = df.collect()
+    # Convert Row objects to CSV format
+    output = StringIO()
+    header = list_row[0].asDict().keys()  # Get the column names from the first row (if the rows are consistent)
+    # Write header
+    output.write(','.join(header) + '\n')
+    # Write each row as a CSV line
+    for row in list_row:
+        row_dict = {k: ("" if v is None else v) for k, v in row.asDict().items()}
+        output.write(','.join(map(str, row_dict.values())) + '\n')
+    # Seek to the beginning of the file-like object to use it
+    output.seek(0)
+    return output
